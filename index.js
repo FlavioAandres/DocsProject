402b8455e03fb409f00e38a257ab08346fe41d79
Flavio A <flv.prj@gmail.com>
2019-06-14 10:12:13 -0500

    completed styles

--- index.js
+++ index.js
@@ -1,44 +1,66 @@
 let docx = require("docx")
-let { Table } = require("docx")
+let { Table, TableCellBorders } = require("docx")
 let { WidthType } = require("docx")
 let fs = require("fs");
-let { exampleList } = require('./src/const')
+let { exampleList, textConstants } = require('./src/const')
 let utils = require('./src/utils')
 let doc = new docx.Document();
 
-let newTextStyled = string => new docx.TextRun(string).font('Arial').size(11 * 2)
+const newTextStyled = (string, size = 11, color = '000') => new docx.TextRun(string).font('Arial').size(size * 2).color(color)
+const setEmptyBorder = () => new TableCellBorders()
+    .addBottomBorder('none', 0, 'white')
+    .addTopBorder('none', 0, 'white')
+
+const setFullBorder = () => new TableCellBorders().addBottomBorder().addEndBorder().addTopBorder('none', 0, 'white')
 
 //Pre Requisites 
+const createHeadersAndFooters = () => {
+    doc.Header.createParagraph(
+        newTextStyled(textConstants.header, 14, '6b0b20')
+        .bold()
+        .underline("single", "6b0b20")
+    );
+    doc.Footer.createParagraph(
+        newTextStyled(textConstants.footer, 10, '6b0b20')
+        .bold()
+    );
+}
 
-const createTable = (boardList) =>{
+const createTable = (boardList) => {
     let rows = utils.getTotalRows(boardList) + 2
 
     //Table creation
     let table = new Table(rows, 4);
-        table.setWidth(WidthType.PERCENTAGE, '100%')
-             .getRow(0)
-             .mergeCells(0, 4)
-             .createParagraph()
-             .addRun(newTextStyled(boardList.BoardName))
+    table.setWidth(WidthType.PERCENTAGE, '100%')
+        .getRow(0)
+        .mergeCells(0, 4)
+        .createParagraph()
+        .addRun(newTextStyled(boardList.BoardName).bold())
 
-    table.getCell(1, 0).createParagraph().addRun(newTextStyled('PROFESSION'))
-    table.getCell(1, 1).createParagraph().addRun(newTextStyled('PROFESSION CODE'))
-    table.getCell(1, 2).createParagraph().addRun(newTextStyled('SUBJECT AREA'))
-    table.getCell(1, 3).createParagraph().addRun(newTextStyled('SUBJECT AREA CODE'))
+    table.getCell(1, 0).createParagraph()
+        .addRun(newTextStyled('Profession').bold())
+
+    table.getCell(1, 1).createParagraph()
+        .addRun(newTextStyled('Profession Code').bold())
+
+    table.getCell(1, 2).createParagraph()
+        .addRun(newTextStyled('Subject Area').bold())
+
+    table.getCell(1, 3).createParagraph()
+        .addRun(newTextStyled('Subject Area code').bold())
 
     return table
-
-
 }
 
 
 const writeState = (stateInformation) => {
-    doc.createParagraph().addRun(newTextStyled(stateInformation.name).bold())
+    createHeadersAndFooters()
+    doc.createParagraph().addRun(newTextStyled(stateInformation.name, 14).bold())
     doc.createParagraph('\n')
     stateInformation.boards
-        .map(board=>createTable(board))
-        .map((table,i) => writeBoardTable(table,stateInformation.boards[i]))
-        .map(table=>{
+        .map(board => createTable(board))
+        .map((table, i) => writeBoardTable(table, stateInformation.boards[i]))
+        .map(table => {
             doc.addTable(table)
             doc.createParagraph('\n')
         })
@@ -51,16 +73,25 @@ const writeBoardTable = (table, boardInfo) => {
     let initRow = 2
     try {
         boardInfo.professions.map(profession => {
+            let pointer = 2
             table.getCell(initRow, 0).createParagraph().addRun(newTextStyled(profession.name))
             table.getCell(initRow, 1).createParagraph().addRun(newTextStyled(profession.code))
-            profession.subjectAreas.forEach((subjectArea, i) => {
+            profession.subjectAreas.forEach(subjectArea => {
+                table.getCell(initRow, 0).root[0].root[0] = setEmptyBorder()
+                table.getCell(initRow, 1).root[0].root[0] = setEmptyBorder()
+                if (profession.subjectAreas.length + 1 === pointer) {
+                    table.getCell(initRow, 1).root[0].root[0] = setFullBorder()
+                    table.getCell(initRow, 0).root[0].root[0] = setFullBorder()
+                }
                 table.getCell(initRow, 2).createParagraph().addRun(newTextStyled(subjectArea.code))
                 table.getCell(initRow, 3).createParagraph().addRun(newTextStyled(subjectArea.name))
                 initRow++
+                pointer++
             })
         });
     } catch (error) {
-        throw new Error(error)
+        console.error(error)
+        return table
     }
     return table
 }
--- src/const/index.js
+++ src/const/index.js
@@ -1,44 +1,108 @@
 module.exports = {
+    textConstants: {
+        header: 'CE Broker: Profession and Subject Area Codes rev. 2.0',
+        footer: ' © 2012 Information Systems of Florida'
+    },
     exampleList: {
-        state:{
+        state: {
             name: 'Florida State',
-            boards:[{
-                BoardName: 'Florida Board of Optometry (partial credit not allowed by this Board)',
-                professions: [{
-                        name: 'Certified Optometris',
-                        code: 'OPC',
-                        subjectAreas: [
-                            { name: 'HIV/AIDS', code: 'HA' },
-                            { name: 'Medical Error', code: 'ME' },
-                            { name: 'General', code: 'GN' },
-                            { name: 'General (Practice Management)', code: 'JP' },
-                            { name: 'General (CPR)', code: 'CP' },
-                        ]
-                    },
-        
-                ]
-                
-            },
-            {
-                BoardName: 'Florida Board of Optometry (partial credit not allowed by this Board)',
-                professions: [{
-                        name: 'Certified Optometris',
-                        code: 'OPC',
-                        subjectAreas: [
-                            { name: 'HIV/AIDS', code: 'HA' },
-                            { name: 'Medical Error', code: 'ME' },
-                            { name: 'General', code: 'GN' },
-                            { name: 'General (Practice Management)', code: 'JP' },
-                            { name: 'General (CPR)', code: 'CP' },
-                            { name: 'Laws and Rules of the Board', code: 'LB' },
-                            { name: 'Prescribing Controlled Substances', code: 'PCSFL' },
-                        ]
-                    },
-        
-                ]
-                
-            }
-        ]
+            boards: [{
+                    BoardName: 'Florida Board of Optometry (partial credit not allowed by this Board)',
+                    professions: [{
+                            name: 'Certified Optometrist',
+                            code: 'OPC',
+                            subjectAreas: [
+                                { name: 'HIV/AIDS', code: 'HA' },
+                                { name: 'Medical Error', code: 'ME' },
+                                { name: 'General', code: 'GN' },
+                                { name: 'General (Practice Management)', code: 'JP' },
+                                { name: 'General (CPR)', code: 'CP' },
+                            ]
+                        },
+
+                    ]
+
+                },
+                {
+                    BoardName: 'Florida Board of Optometry (partial credit not allowed by this Board)',
+                    professions: [{
+                            name: 'Certified Optometrist',
+                            code: 'OPC',
+                            subjectAreas: [
+                                { name: 'HIV/AIDS', code: 'HA' },
+                                { name: 'Medical Error', code: 'ME' },
+                                { name: 'General', code: 'GN' },
+                                { name: 'General (Practice Management)', code: 'JP' },
+                                { name: 'General (CPR)', code: 'CP' },
+                            ]
+                        },
+
+                    ]
+
+                },
+                {
+                    BoardName: 'Florida Board of Optometry (partial credit not allowed by this Board)',
+                    professions: [{
+                            name: 'Certified Optometrist',
+                            code: 'OPC',
+                            subjectAreas: [
+                                { name: 'HIV/AIDS', code: 'HA' },
+                                { name: 'Medical Error', code: 'ME' },
+                                { name: 'General', code: 'GN' },
+                                { name: 'General (Practice Management)', code: 'JP' },
+                                { name: 'General (CPR)', code: 'CP' },
+                            ]
+                        },
+
+                    ]
+
+                },
+                {
+                    BoardName: 'Florida Board of Optometry (partial credit not allowed by this Board)',
+                    professions: [{
+                            name: 'Certified Optometrist',
+                            code: 'OPC',
+                            subjectAreas: [
+                                { name: 'HIV/AIDS', code: 'HA' },
+                                { name: 'Medical Error', code: 'ME' },
+                                { name: 'General', code: 'GN' },
+                                { name: 'General (Practice Management)', code: 'JP' },
+                                { name: 'General (CPR)', code: 'CP' },
+                                { name: 'Laws and Rules of the Board', code: 'LB' },
+                                { name: 'Prescribing Controlled Substances', code: 'PCSFL' },
+                            ]
+                        },
+                        {
+                            name: 'Certified Optometrist',
+                            code: 'OPC',
+                            subjectAreas: [
+                                { name: 'HIV/AIDS', code: 'HA' },
+                                { name: 'Medical Error', code: 'ME' },
+                                { name: 'General', code: 'GN' },
+                                { name: 'General (Practice Management)', code: 'JP' },
+                                { name: 'General (CPR)', code: 'CP' },
+                                { name: 'Laws and Rules of the Board', code: 'LB' },
+                                { name: 'Prescribing Controlled Substances', code: 'PCSFL' },
+                            ]
+                        },
+                        {
+                            name: 'Certified Optometrist',
+                            code: 'OPC',
+                            subjectAreas: [
+                                { name: 'HIV/AIDS', code: 'HA' },
+                                { name: 'Medical Error', code: 'ME' },
+                                { name: 'General', code: 'GN' },
+                                { name: 'General (Practice Management)', code: 'JP' },
+                                { name: 'General (CPR)', code: 'CP' },
+                                { name: 'Laws and Rules of the Board', code: 'LB' },
+                                { name: 'Prescribing Controlled Substances', code: 'PCSFL' },
+                            ]
+                        },
+
+                    ]
+
+                }
+            ]
         }
 
     }
